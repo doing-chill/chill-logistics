16c0dfbc9b353a7e97d71778a3947cd25702cd13
kimsj0970 <kimsj0970@naver.com>
2025-11-28 11:39:44 +0900

    [FEAT] Hub 모듈 추가 및 초기 세팅

---
+++ hub-server/src/test/java/chill_logistics/hub_server/HubServerApplicationTests.java
@@ -0,0 +1,13 @@
+package chill_logistics.hub_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
