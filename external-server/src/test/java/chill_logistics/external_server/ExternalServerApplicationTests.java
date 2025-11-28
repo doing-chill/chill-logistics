186ccc556c330c9e1996324c103fbfd57dc9aacb
kimsj0970 <kimsj0970@naver.com>
2025-11-28 11:40:58 +0900

    [FEAT] External 모듈 추가 및 초기 세팅

---
+++ external-server/src/test/java/chill_logistics/external_server/ExternalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package chill_logistics.external_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
