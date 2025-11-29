361c38a4757a731d3bde7166e69447dea1d2b5c4
seolbin01 <se01bin@suwon.ac.kr>
2025-11-30 06:41:08 +0900

    [FEAT] Hub 관련 엔티티 구현

---
+++ hub-server/src/main/java/chill_logistics/hub_server/domain/entity/HubRouteLog.java
@@ -0,0 +1,36 @@
+package chill_logistics.hub_server.domain.entity;
+
+import jakarta.persistence.*;
+import lib.entity.BaseEntity;
+import lombok.Getter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Entity
+@Table(name = "p_hub_route_log")
+public class HubRouteLog extends BaseEntity {
+
+    @Id
+    @GeneratedValue(generator = "uuidv7")
+    @GenericGenerator(
+            name = "uuidv7",
+            strategy = "lib.id.UUIDv7Generator"
+    )
+    @Column(name = "id", columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @Column(name = "start_hub_id", nullable = false, columnDefinition = "BINARY(16)")
+    private UUID startHubId;
+
+    @Column(name = "end_hub_id", nullable = false, columnDefinition = "BINARY(16)")
+    private UUID endHubId;
+
+    @Column(name = "total_duration")
+    private Integer totalDuration;
+
+    @Column(name = "total_distance", precision = 10, scale = 3)
+    private BigDecimal totalDistance;
+}
