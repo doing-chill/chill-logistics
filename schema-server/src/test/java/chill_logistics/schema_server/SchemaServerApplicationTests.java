7d2ef553c47abb83a3bb373902fb7273a7f20e1e
seolbin01 <se01bin@suwon.ac.kr>
2025-11-29 19:33:57 +0900

    [CHORE] schema-server 모듈 생성

---
+++ schema-server/src/test/java/chill_logistics/schema_server/SchemaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package chill_logistics.schema_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchemaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
