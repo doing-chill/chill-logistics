5fc0e610e321360b925b3863125a1f3c01e63814
seolbin01 <se01bin@suwon.ac.kr>
2025-12-02 16:20:44 +0900

    [FEAT] PRODUCT_NOT_FOUND 에러 코드 추가

--- product-server/src/main/java/chill_logistics/product_server/lib/exception/ErrorCode.java
+++ product-server/src/main/java/chill_logistics/product_server/lib/exception/ErrorCode.java
@@ -8,10 +8,10 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode implements BaseErrorCode {
 
     // 403
-    FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN, "접근 권한이 없습니다.")
+    FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
 
-    ;
-
+    // 404
+    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 상품을 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
