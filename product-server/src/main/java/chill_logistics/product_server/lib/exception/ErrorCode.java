60715fbfe09efa64c99690fc6a7d3d3671ba8088
seolbin01 <se01bin@suwon.ac.kr>
2025-12-02 15:28:14 +0900

    [REFACTOR] ErrorCode 패키지 이동

---
+++ product-server/src/main/java/chill_logistics/product_server/lib/exception/ErrorCode.java
@@ -0,0 +1,23 @@
+package chill_logistics.product_server.lib.exception;
+
+import lib.web.error.BaseErrorCode;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode implements BaseErrorCode {
+
+    // 403
+    FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN, "접근 권한이 없습니다.")
+
+    ;
+
+
+    private final HttpStatus status;
+    private final String message;
+
+    ErrorCode(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
