736ceee578afbd62eeed79ee04522eab88228749
kookong2 <hy010309@naver.com>
2025-12-04 01:34:25 +0900

    [FEAT] JpaAuditing 추가

--- user-server/src/main/java/chill_logistics/user_server/UserServerApplication.java
+++ user-server/src/main/java/chill_logistics/user_server/UserServerApplication.java
@@ -3,6 +3,7 @@ package chill_logistics.user_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(
     scanBasePackages = {
@@ -13,6 +14,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
         "lib.jwt"
     }
 )
+@EnableJpaAuditing
 @EnableDiscoveryClient
 public class UserServerApplication {
 
