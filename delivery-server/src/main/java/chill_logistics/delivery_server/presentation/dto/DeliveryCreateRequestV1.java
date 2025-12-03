63dde7c6dec9b6ac551466100671a3bbc7c7138a
Jess <jcscene@gmail.com>
2025-12-03 18:09:51 +0900

    [FIX] CompileError 해결 DeliveryCreateRequestV1에서도 HubRouteAfterCommandV1 받아오도록 수정

--- delivery-server/src/main/java/chill_logistics/delivery_server/presentation/dto/DeliveryCreateRequestV1.java
+++ delivery-server/src/main/java/chill_logistics/delivery_server/presentation/dto/DeliveryCreateRequestV1.java
@@ -1,13 +1,13 @@
 package chill_logistics.delivery_server.presentation.dto;
 
-import chill_logistics.delivery_server.infrastructure.kafka.dto.HubRouteAfterCreateV1;
+import chill_logistics.delivery_server.application.dto.command.HubRouteAfterCommandV1;
 import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 
 public record DeliveryCreateRequestV1(
 
     @NotNull
-    HubRouteAfterCreateV1 orderInfo,
+    HubRouteAfterCommandV1 orderInfo,
 
     @NotNull
     UUID hubDeliveryPersonId,
