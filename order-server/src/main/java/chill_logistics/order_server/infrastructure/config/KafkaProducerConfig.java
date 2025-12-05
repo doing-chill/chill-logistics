f263858f0d8055dfddf0de6b2a4431109351ed9b
seolbin01 <se01bin@suwon.ac.kr>
2025-12-06 08:14:14 +0900

    [FEAT] Kafka producer 설정 추가

---
+++ order-server/src/main/java/chill_logistics/order_server/infrastructure/config/KafkaProducerConfig.java
@@ -0,0 +1,40 @@
+package chill_logistics.order_server.infrastructure.config;
+
+import chill_logistics.order_server.infrastructure.kafka.dto.OrderAfterCreateV1;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Bean
+    public ProducerFactory<String, OrderAfterCreateV1> orderAfterCreateProducerFactory() {
+
+        Map<String, Object> props = new HashMap<>();
+
+        // Kafka Broker
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
+        // 직렬화 설정
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        // 필요시 기타 옵션 (acks, retries 등) 추가
+
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, OrderAfterCreateV1> orderAfterCreateKafkaTemplate() {
+        return new KafkaTemplate<>(orderAfterCreateProducerFactory());
+    }
+}
