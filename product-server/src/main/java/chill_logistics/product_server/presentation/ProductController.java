b272fc8b241e62dbf5b813a81025159f3ccd406c
seolbin01 <se01bin@suwon.ac.kr>
2025-12-10 23:31:54 +0900

    [DOCS] 주문 관련 api swagger 어노테이션 추가

--- product-server/src/main/java/chill_logistics/product_server/presentation/ProductController.java
+++ product-server/src/main/java/chill_logistics/product_server/presentation/ProductController.java
@@ -1,11 +1,14 @@
 package chill_logistics.product_server.presentation;
 
 import chill_logistics.product_server.application.ProductFacade;
-import chill_logistics.product_server.presentation.dto.request.*;
+import chill_logistics.product_server.presentation.dto.request.CreateProductRequestV1;
+import chill_logistics.product_server.presentation.dto.request.ReadProductRequestV1;
+import chill_logistics.product_server.presentation.dto.request.UpdateProductRequestV1;
 import chill_logistics.product_server.presentation.dto.response.CreateProductResponseV1;
 import chill_logistics.product_server.presentation.dto.response.ReadProductDetailResponseV1;
-import chill_logistics.product_server.presentation.dto.response.ReadProductInternalResponseV1;
 import chill_logistics.product_server.presentation.dto.response.ReadProductSummaryResponseV1;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lib.entity.BaseStatus;
 import lib.web.response.BaseResponse;
@@ -22,6 +25,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/v1/products")
 @RequiredArgsConstructor
+@Tag(name = "1. 상품 관리", description = "상품 관리용 API")
 public class ProductController {
 
     private final ProductFacade productFacade;
@@ -30,6 +34,7 @@ public class ProductController {
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
     @PreAuthorize("hasAnyRole('MASTER', 'HUB_MANAGER', 'FIRM_MANAGER')")
+    @Operation(summary = "상품 추가", description = "상품 추가 API입니다.")
     public BaseResponse<CreateProductResponseV1> createProduct(
             @RequestBody @Valid CreateProductRequestV1 request) {
 
@@ -47,6 +52,7 @@ public class ProductController {
     @PatchMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("hasAnyRole('MASTER', 'HUB_MANAGER', 'FIRM_MANAGER')")
+    @Operation(summary = "상품 정보 수정", description = "상품 정보 수정 API입니다.")
     public BaseResponse<Void> updateProduct(
             @PathVariable UUID id,
             @RequestBody @Valid UpdateProductRequestV1 request) {
@@ -60,6 +66,7 @@ public class ProductController {
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("hasAnyRole('MASTER', 'HUB_MANAGER')")
+    @Operation(summary = "상품 삭제", description = "상품 삭제 API입니다.")
     public BaseResponse<Void> deleteProduct(@PathVariable UUID id) {
 
         productFacade.deleteProduct(id);
@@ -71,6 +78,7 @@ public class ProductController {
     @GetMapping()
     @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("hasAnyRole('MASTER', 'HUB_MANAGER', 'DELIVERY_MANAGER', 'FIRM_MANAGER')")
+    @Operation(summary = "상품 목록 조회", description = "상품 목록 조회 API입니다.")
     public BaseResponse<List<ReadProductSummaryResponseV1>> readProductList(
             @ModelAttribute ReadProductRequestV1 request) {
 
@@ -88,6 +96,7 @@ public class ProductController {
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     @PreAuthorize("hasAnyRole('MASTER', 'HUB_MANAGER', 'DELIVERY_MANAGER', 'FIRM_MANAGER')")
+    @Operation(summary = "상품 단건 조회", description = "상품 단건 조회 API입니다.")
     public BaseResponse<ReadProductDetailResponseV1> readProduct(@PathVariable UUID id) {
 
         ReadProductDetailResponseV1 response =
@@ -96,45 +105,5 @@ public class ProductController {
                 );
 
         return BaseResponse.ok(response, BaseStatus.OK);
-    }
-
-    /* 상품 단건 조회 (내부 API) */
-    @GetMapping("/internal/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    @PreAuthorize("hasAnyRole('MASTER', 'FIRM_MANAGER')")
-    public BaseResponse<ReadProductInternalResponseV1> readProductInternal(@PathVariable UUID id) {
-
-        ReadProductInternalResponseV1 response =
-                ReadProductInternalResponseV1.from(
-                        productFacade.readProductInternal(id)
-                );
-
-        return BaseResponse.ok(response, BaseStatus.OK);
-    }
-
-    /* 재고 차감 (내부 API) */
-    @PutMapping("/internal/{id}/decrease")
-    @ResponseStatus(HttpStatus.OK)
-    @PreAuthorize("hasAnyRole('MASTER', 'FIRM_MANAGER')")
-    public BaseResponse<Void> decreaseStockInternal(
-            @PathVariable UUID id,
-            @RequestBody @Valid UpdateStockDecreaseRequestV1 request) {
-
-        productFacade.decreaseStockInternal(request.toCommand(id));
-
-        return BaseResponse.ok(BaseStatus.OK);
-    }
-
-    /* 재고 복원 (내부 API) */
-    @PutMapping("/internal/{id}/recover")
-    @ResponseStatus(HttpStatus.OK)
-    @PreAuthorize("hasAnyRole('MASTER', 'HUB_MANAGER', 'FIRM_MANAGER')")
-    public BaseResponse<Void> recoveryStockInternal(
-            @PathVariable UUID id,
-            @RequestBody @Valid UpdateStockRecoverRequestV1 request) {
-
-        productFacade.recoverStockInternal(request.toCommand(id));
-
-        return BaseResponse.ok(BaseStatus.OK);
     }
 }
