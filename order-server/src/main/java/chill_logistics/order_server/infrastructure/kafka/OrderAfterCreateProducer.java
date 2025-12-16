2143a8a372b1bf1975e6eb1961a7677f2fd4b555
Jess <jcscene@gmail.com>
2025-12-16 15:30:59 +0900

    [FEAT] order-server EventPublisher에 orderStatusChangedTopic 추가

--- order-server/src/main/java/chill_logistics/order_server/infrastructure/kafka/OrderAfterCreateProducer.java
+++ order-server/src/main/java/chill_logistics/order_server/infrastructure/kafka/OrderAfterCreateProducer.java
@@ -1,5 +1,6 @@
 package chill_logistics.order_server.infrastructure.kafka;
 
+import chill_logistics.order_server.application.dto.command.OrderStatusChangedV1;
 import chill_logistics.order_server.domain.event.EventPublisher;
 import chill_logistics.order_server.application.dto.command.OrderAfterCreateV1;
 import lombok.RequiredArgsConstructor;
@@ -14,9 +15,13 @@ import org.springframework.stereotype.Service;
 public class OrderAfterCreateProducer implements EventPublisher {
 
     private final KafkaTemplate<String, OrderAfterCreateV1> orderAfterCreateKafkaTemplate;
+    private final KafkaTemplate<String, OrderStatusChangedV1> orderStatusChangedKafkaTemplate;
 
     @Value("${app.kafka.topic.order-after-create}")
     private String orderAfterCreateTopic;
+
+    @Value("${app.kafka.topic.order-status-changed}")
+    private String orderStatusChangedTopic;
 
     /**
      * 주문 생성 후 Kafka로 OrderAfterCreate 이벤트를 발행합니다.
@@ -41,5 +46,29 @@ public class OrderAfterCreateProducer implements EventPublisher {
                                 key, result.getRecordMetadata().offset());
                     }
                 });
+    }
+
+    /* 여기 추가 */
+    /**
+     * 주문 상태 변경(취소/실패/완료 등) 시 Kafka로 OrderStatusChanged 이벤트 발행
+     */
+    @Override
+    public void sendOrderStatusChanged(OrderStatusChangedV1 message) {
+
+        String key = message.orderId().toString();
+
+        log.info("[Kafka] OrderStatusChanged 메시지 발행, topic={}, key={}, message={}",
+            orderStatusChangedTopic, key, message);
+
+        orderStatusChangedKafkaTemplate
+            .send(orderStatusChangedTopic, key, message)
+            .whenComplete((result, ex) -> {
+                if (ex != null) {
+                    log.error("[Kafka] OrderStatusChanged 메시지 전송 실패, key={}", key, ex);
+                } else {
+                    log.info("[Kafka] OrderStatusChanged 메시지 전송 성공, orderId={}, offset={}",
+                        key, result.getRecordMetadata().offset());
+                }
+            });
     }
 }
