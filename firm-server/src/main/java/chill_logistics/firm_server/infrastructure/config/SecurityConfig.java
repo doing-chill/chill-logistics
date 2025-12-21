d759ca802926f57b5cb1dc0cff6180347c67c374
kookong2 <hy010309@naver.com>
2025-12-21 23:57:25 +0900

    [CHORE] 불필요한 코드 삭제

--- firm-server/src/main/java/chill_logistics/firm_server/infrastructure/config/SecurityConfig.java
+++ firm-server/src/main/java/chill_logistics/firm_server/infrastructure/config/SecurityConfig.java
@@ -1,14 +1,10 @@
 package chill_logistics.firm_server.infrastructure.config;
 
-import lib.jwt.JwtTokenProvider;
-import lib.security.JwtAuthenticationFilter;
 import lib.security.PassportAuthenticationFilter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
--- hub-server/src/main/java/chill_logistics/hub_server/infrastructure/config/SecurityConfig.java
+++ hub-server/src/main/java/chill_logistics/hub_server/infrastructure/config/SecurityConfig.java
@@ -1,15 +1,10 @@
 package chill_logistics.hub_server.infrastructure.config;
 
-import java.util.Set;
-import lib.jwt.JwtTokenProvider;
-import lib.security.JwtAuthenticationFilter;
 import lib.security.PassportAuthenticationFilter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
