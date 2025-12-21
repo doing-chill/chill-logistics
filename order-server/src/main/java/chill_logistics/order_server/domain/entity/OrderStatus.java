5dbad22af57d471b3978fef654ae089ca029761b
seolbin01 <se01bin@suwon.ac.kr>
2025-12-22 00:49:47 +0900

    [CHORE] order_status 수정

--- order-server/src/main/java/chill_logistics/order_server/domain/entity/OrderStatus.java
+++ order-server/src/main/java/chill_logistics/order_server/domain/entity/OrderStatus.java
@@ -1,9 +1,11 @@
 package chill_logistics.order_server.domain.entity;
 
 public enum OrderStatus {
-    CREATED,
-    PROCESSING,
-    IN_TRANSIT,
-    COMPLETED,
-    CANCELED
+    CREATED,            // 주문 생성됨
+    STOCK_PROCESSING,   // 재고 처리 중
+    STOCK_CONFIRMED,    // 재고 확보 완료
+    PROCESSING,         // 배송 준비
+    IN_TRANSIT,         // 배송 중
+    COMPLETED,          // 배송 완료
+    CANCELED            // 주문 취소
 }
